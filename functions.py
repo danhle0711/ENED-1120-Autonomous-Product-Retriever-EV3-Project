31779aa6af8e1f5338874ef9671454bcc8073476
Danh Le <77718151+danhle0711@users.noreply.github.com>
2023-04-18 16:38:54 -0400

    edit stuff

---
+++ functions.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env micropython
+from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_D, SpeedPercent, MoveDifferential, follow_for_ms
+from ev3dev2.wheel import EV3Tire
+from ev3dev2.sensor.lego import GyroSensor, ColorSensor, UltrasonicSensor
+import time
+import random
+
+# Initialize constants
+distance_between_wheels = 118
+left_motor = LargeMotor(OUTPUT_A)
+right_motor = LargeMotor(OUTPUT_D)
+lift_motor = MediumMotor(OUTPUT_B)
+DA_diff = MoveDifferential(OUTPUT_D, OUTPUT_A, EV3Tire, distance_between_wheels)
+DA_diff.gyro = GyroSensor()
+DA_diff.gyro.reset()
+DA_diff.gyro.calibrate()
+# DA_diff.gyro.mode = 'GYRO-ANG'
+DA_diff.cs = ColorSensor()
+DA_diff.cs.mode = 'COL-COLOR'
+DA_diff.ultrasonic = UltrasonicSensor()
+DA_diff.ultrasonic.mode = 'US-DIST-CM'
+
+standard_speed_percent = 20
+
+distance_to_first_box = 0.8125 
+box_length = 4 + (3/8)
+distance_between_each_box = 6
+shelf_length = 36
+intersection_length = 12
+
+# I don't really know if this file even works
+
+# Functions
+def go_straight_in(distance_in, speed_percent, target_angle_degree):
+    # Description: drive forward {distance_in} inches with {speed_percent} percentage of max speed
+    
+    # distance_mm = float(input('Enter the distance: '))
+    # speed_percent = float(input('Enter the speed: '))
+
+    # distance_mm = distance_in * 25.4
+
+    # Tinker with this coefficient
+    # coefficient_of_distance = 1
+    # coefficient_of_distance = (1 / 1.25)
+    coefficient_of_distance = 261.54
+
+    # DA_diff.on_for_distance(SpeedPercent(speed_percent), coefficient_of_distance * distance_mm)
+
+    DA_diff.follow_gyro_angle(
+        kp = 11.3, ki = 0.05, kd = 3.2,
+        speed = SpeedPercent(speed_percent),
+        target_angle = target_angle_degree,
+        follow_for = follow_for_ms,
+        ms = coefficient_of_distance * distance_in
+    )
+
+def turn_right_degrees(angle_degrees, speed_percent):
+    # Description: turns {angle_degrees} degrees to the right
+    DA_diff.turn_right(SpeedPercent(speed_percent), angle_degrees, brake = True, block = True, error_margin = 0.1, use_gyro = True)
+
+def turn_left_degrees(angle_degrees, speed_percent):
+    # Description: turns {angle_degrees} degrees to the left
+
+    DA_diff.turn_left(SpeedPercent(speed_percent), angle_degrees, brake = True, block = True, error_margin = 0.1, use_gyro = True)
+
+def instructions_to_list(input_list):
+    shelf = input_list[0][:2]
+    location_in_shelf = int(input_list[0][3])
+    barcode_type = int(input_list[1])
+    fulfillment_area = input_list[2]
+    return [shelf, location_in_shelf, barcode_type, fulfillment_area]
+
+def check_accuracy():
+    print('Gyro value: {0}'.format(DA_diff.gyro.value()))
+    print('Left motor: {0}'.format(left_motor.position))
+    print('Right motor: {0}'.format(right_motor.position))
+    print('Lift motor: {0}'.format(lift_motor.position))
+    # print('\n')
+
+def reset_motors():
+    left_motor.reset()
+    right_motor.reset()
+    lift_motor.reset()
+
+def barcode_reader():
+    code_list = [9, 9, 9, 9]
+    for i in range(4):
+        time.sleep(2)
+        if (DA_diff.cs.color == 1):
+            code_list[i] = 1
+        else:
+            code_list[i] = 0
+        print(code_list)
+        go_straight_in(0.80, 30, 0)
+    return code_list
+
+def barcode_type_identifier(code_list):
+    if (code_list == [0, 0, 0, 1] or code_list == [1, 0, 0, 0]):
+        result = 1
+    elif (code_list == [0, 1, 0, 1] or code_list == [1, 0, 1, 0]):
+        result = 2
+    elif (code_list == [0, 0, 1, 1] or code_list == [1, 1, 0, 0]):
+        result = 3
+    elif (code_list == [1, 0, 0, 1]):
+        result = 4
+    else:
+        print('Error, making random guess')
+        result = random.randint(1, 4)
+    print('Barcode type: {0}'.format(result))
+    return result
